cdb94943cb1e03a1fe816db452043de971ab4dfb
Jose Fernando Álvarez Ferragut <jose-fernando.alvarez-ferragut@capgemini.com>
2023-03-10 13:31:23 +0100

    Transloco support added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpErrorInterceptor } from './interceptors/http.error.interceptor';
 import { AppUnauthorizedComponent } from './components/app-unauthorized/app-unauthorized.component';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
 	declarations: [AppComponent, AppUnauthorizedComponent],
-	imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+	imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {}), HttpClientModule, TranslocoRootModule],
 	providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }],
 	bootstrap: [AppComponent],
 })
