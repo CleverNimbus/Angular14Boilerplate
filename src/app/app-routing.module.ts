a84b0a2ad02ecb0fb912d4a76edef5c0d064d83a
Jose Fernando Álvarez Ferragut <jose-fernando.alvarez-ferragut@capgemini.com>
2023-03-10 13:48:57 +0100

    Linting configuration License added Lint issues solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppUnauthorizedComponent } from './components/app-unauthorized/app-unauthorized.component';
 
 const routes: Routes = [
-	{ path: "unauthorized", component: AppUnauthorizedComponent, title: 'Unauthorized'}
+	{ path: 'unauthorized', component: AppUnauthorizedComponent, title: 'Unauthorized'}
 ];
 
 @NgModule({
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,16 +2,16 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export abstract class HttpService {
-	public readonly HOST_URL: string = environment.apiUrl;
-	protected readonly httpClient: HttpClient;
-
 	httpOptions = {
 		headers: new HttpHeaders({
 			'Content-Type': 'application/json',
 		}),
 	};
 
+	public readonly hostUrl: string = environment.apiUrl;
+	protected readonly httpClient: HttpClient;
+
 	constructor(httpClient: HttpClient) {
 		this.httpClient = httpClient;
-	}    
+	}
 }
